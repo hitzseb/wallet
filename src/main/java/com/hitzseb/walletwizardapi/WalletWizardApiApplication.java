100906aa269997f959ef41dbac20e23031c06d46
hitzseb <hitzseb@gmail.com>
2023-07-05 20:25:33 -0300

    swagger fix

--- src/main/java/com/hitzseb/walletwizardapi/WalletWizardApiApplication.java
+++ src/main/java/com/hitzseb/walletwizardapi/WalletWizardApiApplication.java
@@ -1,9 +1,15 @@
 package com.hitzseb.walletwizardapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Wallet Wizard API",
+        version = "1.0", description = "Backend for Wallet Wizard web application"))
 public class WalletWizardApiApplication {
 
     public static void main(String[] args) {
